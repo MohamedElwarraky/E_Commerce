2303e17aa74e39bb8344d37131d05a903c1eb5b1
Mohamed Elwarraky <ModyElwarraky@gmail.com>
2021-01-17 16:52:08 +0200

    Finishing CartItem UI

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 const KMainColor = Color(0xFFFFC12F);
+const KSecondaryColor = Colors.orangeAccent;
 const KEmailTextFieldColor = Color(0xFFFFE6AC);
 const kProductName = 'productName';
 const kProductPrice = 'productPrice';
--- lib/screens/user/cart_screen.dart
+++ lib/screens/user/cart_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_app/constants.dart';
 import 'package:flutter_app/models/product.dart';
 import 'package:flutter_app/provider/cartItem.dart';
 import 'package:provider/provider.dart';
@@ -9,14 +11,133 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<Product> products = Provider.of<CartItem>(context).products;
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double appBarHeight = AppBar().preferredSize.height;
+    final double statusBarHeight = MediaQuery.of(context).padding.top;
     return Scaffold(
-      body: ListView.builder(
-          itemCount: products.length,
-          itemBuilder: (context, index) {
-            return Container(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+        ),
+        title: Text(
+          'My Cart',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Column(
+        children: [
+          LayoutBuilder(builder: (context, constrains) {
+            if (products.isNotEmpty) {
+              return Container(
+                height: screenHeight -
+                    appBarHeight -
+                    statusBarHeight -
+                    (screenHeight * 0.08),
+                child: ListView.builder(
+                    itemCount: products.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: Container(
+                          height: screenHeight * 0.15,
+                          color: KSecondaryColor,
+                          child: Row(
+                            children: [
+                              CircleAvatar(
+                                backgroundImage:
+                                    AssetImage(products[index].pLocation),
+                                radius: screenHeight * 0.15 / 2,
+                              ),
+                              Expanded(
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 10),
+                                      child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Text(
+                                              products[index].pName,
+                                              style: TextStyle(
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            SizedBox(
+                                              height: 10,
+                                            ),
+                                            Text(
+                                              '\$ ${products[index].pPrice}',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            )
+                                          ]),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(right: 20),
+                                      child: Text(
+                                        products[index].pQuantity.toString(),
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    }),
+              );
+            }
 
+            return Container(
+              height: screenHeight -
+                  (screenHeight * 0.08) -
+                  statusBarHeight -
+                  appBarHeight,
+              child: Center(
+                child: Text('Cart is empty!'),
+              ),
             );
           }),
+          ButtonTheme(
+            minWidth: screenWidth,
+            height: screenHeight * 0.08,
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(
+                    10,
+                  ),
+                ),
+              ),
+              onPressed: () {},
+              child: Text(
+                'Order'.toUpperCase(),
+              ),
+              color: KMainColor,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
